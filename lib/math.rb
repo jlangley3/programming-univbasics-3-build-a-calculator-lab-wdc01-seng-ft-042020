f93de735c798f39b0604f38a44fc994b27d049bc
Jesse Langley <jlangleyiii@aol.com>
2020-04-08 17:47:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,5 @@
 def addition(num1, num2)
-answer = (num1 + num2)
+answer = (num2 + num1)
 puts answer
 answer
 end
@@ -11,25 +11,25 @@ answer
 end
 
 def division(num1, num2)
-answer = (num1 / num2)
+answer = (num2 / num1)
 puts answer
 answer
 end
 
 def multiplication(num1, num2)
-answer = (num1 - num2)
+answer = (num1 * num2)
 puts answer
 answer
 end
 
 def modulo(num1, num2)
-answer = (num1 - num2)
+answer = (num2 % num1)
 puts answer
 answer
 end
 
 def square_root(num)
-answer = (num1 - num2)
+answer = math.sqrt(num)
 puts answer
 answer
 end
